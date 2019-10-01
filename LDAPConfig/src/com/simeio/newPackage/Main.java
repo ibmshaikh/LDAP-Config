4253031430b0a6ff80cb522d7aa2339627a4f2a1
ishaikh <ishaikh@IShaikh-PC.simeiosolutions.com>
2019-10-01 13:50:04 +0530

    New file added

---
+++ LDAPConfig/src/com/simeio/newPackage/Main.java
@@ -0,0 +1,12 @@
+package com.simeio.newPackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello friend chai pee loo");
+
+	}
+
+}
